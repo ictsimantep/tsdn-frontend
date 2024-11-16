1715410997fa0a8c1d7ab9986c07c9d48a0a3683
Egi andriadi <egi.andriadi@gmail.com>
2024-11-17 00:06:52 +0700

    Remove health

--- src/app/[lang]/(dashboard)/(private)/admin/healths/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React from "react";
-import ViewAdminHealths from "@views/admin/healths/Index";
-
-export default function AdminHealthsPage() {
-  return <ViewAdminHealths />;
-}
--- src/hooks/forms/admin/useGetHealths.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { findHealths } from "../../../api/admin/healths";
-
-export const useGetHealths = (id: string) =>
-  useQuery({
-    queryKey: ["get-healths-data", id],
-    queryFn: () => findHealths(id),
-    enabled: !!id,
-  });
